c56bec51e956e9cffcb7777273dc66b834534872
Lee DongWook <ledu202@gmail.com>
2023-10-10 23:34:58 +0900

    knife_detection

---
+++ knife_detection/video.py
@@ -0,0 +1,17 @@
+import cv2
+
+def get_stream_video():
+    # camera 정의
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        # 카메라 값 불러오기
+        success, frame = cam.read()
+
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(frame) + b'\r\n')
